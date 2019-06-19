5660f1be539e4b86dec2856a3b228fe416a9e8fd
王江民 <1778682202@qq.com>
2019-06-19 18:23:06 +0800

    eclipse 提交   001

--- SpringBoot_Demo/src/main/java/demo_app/helloWorld.java
+++ SpringBoot_Demo/src/main/java/demo_app/helloWorld.java
@@ -6,6 +6,7 @@ public class helloWorld  {
 	
 	public static void main(String [] args) {
 		System.out.println("你好 世界 !!");
-		System.out.println("第一次提交代码  001 ");		
+		System.out.println("第一次提交代码  001 ");
+		System.out.println("你好");
 	}
 }
