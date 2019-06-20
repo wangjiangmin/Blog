65d91df2d86192ab5ad08f85f37fd71a7de92ba0
王江民 <1778682202@qq.com>
2019-06-20 12:06:17 +0800

    添加分支修改内容 提交

--- SpringBoot_Demo/src/main/java/demo_app/helloWorld.java
+++ SpringBoot_Demo/src/main/java/demo_app/helloWorld.java
@@ -6,6 +6,7 @@ public class helloWorld  {
 	
 	public static void main(String [] args) {
 		System.out.println("你好 世界 !!");
-		System.out.println("第一次提交代码  001 ");		
+		System.out.println("第一次提交代码  001 ");
+		//分支合并信息 添加  001
 	}
 }
